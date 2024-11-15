bc13a0014ada79046dbfc3b2486bc97dd2a1ec17
Vaishnavi Bhargava <f2015777@pilani.bits-pilani.ac.in>
2024-11-15 15:01:41 -0600

    adding batched matmul

--- cuda/matmul.cu
+++ cuda/matmul.cu
@@ -1,27 +1,56 @@
 #include "matmul.cuh"
 
-__global__ void scaled_matmul(const float *m1, const float *m2, float* out, unsigned int r1, unsigned int c1, unsigned int c2, float factor){
+__global__ void scaled_matmul(const float *m1, const float *m2, float *out, unsigned int r1, unsigned int c1, unsigned int c2, float factor)
+{
     int r = blockIdx.x * blockDim.x + threadIdx.x;
     int c = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if(r < r1 && c < c2) {
+    if (r < r1 && c < c2)
+    {
         float value = 0.0;
-        for(int i = 0; i < c1; i++){
+        for (int i = 0; i < c1; i++)
+        {
             value += m1[r * c1 + i] * m2[i * c2 + c];
         }
         out[r * c2 + c] = value * factor;
     }
 }
 
-__global__ void scaled_matmul_transposed(const float *m1, const float *m2, float* out, unsigned int r1, unsigned int r2, unsigned int c1, float factor){
+__global__ void scaled_matmul_transposed(const float *m1, const float *m2, float *out, unsigned int r1, unsigned int r2, unsigned int c1, float factor)
+{
     int r = blockIdx.x * blockDim.x + threadIdx.x;
     int c = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if(r < r1 && c < r2) {
+    if (r < r1 && c < r2)
+    {
         float value = 0.0;
-        for(int i = 0; i < c1; i++){
+        for (int i = 0; i < c1; i++)
+        {
             value += m1[r * c1 + i] * m2[c * c1 + i];
         }
         out[r * r2 + c] = value * factor;
     }
+}
+
+__global__ void scaled_batched_matmul(const float *q, const float *k, float *out, unsigned int B, unsigned int T, unsigned int C, unsigned int NH, factor)
+{
+    // sz of output is B*NH*T*T
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index >= B * NH * T * T)
+        return;
+    int batch = index / NH * T * T;
+    int rem1 = index % (NH * T * T);
+    int head = rem1 / (T * T);
+    int rem2 = rem1 % (T * T);
+    int row = rem2 / T;
+    int col = rem2 % T;
+    int hs = C / NH;
+    float val = 0.0;
+    float *q_start = q + batch * T * C + head * hs + row * NH * hs;
+    float *k_start = k + batch * T * C + head * hs + col * NH * hs;
+    for (int c = 0; c < hs; c++)
+    {
+        val += q_start[c] * k_start[c];
+    }
+    out[index] = val;
 }
--- cuda/test.cu
+++ cuda/test.cu
@@ -1,47 +1,69 @@
 #include "attention.cu"
 #include <iostream>
 
-#define L 3
+#define L 2
 #define D 2
 
-int main() {
+int main()
+{
+    const unsigned int B = 1;  // Batch size
+    const unsigned int T = 2;  // Sequence length
+    const unsigned int C = 2;  // Feature dimension
+    const unsigned int NH = 1; // Number of heads
+
     // Host matrices
-    float h_Q[L * D] = {1, 2, 3, 4, 5, 6};  // 2x3 matrix
-    float h_K[L * D] = {7, 8, 9, 10, 11, 12};  // 2x3 matrix
-    float h_V[L * D] = {1, 2, 3, 4, 5, 6};  // 2x3 matrix
-    float h_output[L * D];
-    
+    // float h_Q[L * D] = {1, 2, 3, 4, 5, 6};    // 2x3 matrix
+    // float h_K[L * D] = {7, 8, 9, 10, 11, 12}; // 2x3 matrix
+    // float h_V[L * D] = {1, 2, 3, 4, 5, 6};    // 2x3 matrix
+    float h_output[B * T * T * NH];
+
+    float h_q[] = {
+        1.0f, 2.0f, // Q[0][0]
+        3.0f, 4.0f  // Q[0][1]
+    };
+
+    float h_k[] = {
+        5.0f, 6.0f, // K[0][0]
+        7.0f, 8.0f  // K[0][1]
+    };
+
     // Device matrices
     float *d_Q, *d_K, *d_V, *d_output;
-    cudaMalloc(&d_Q, L * D * sizeof(float));
-    cudaMalloc(&d_K, L * D * sizeof(float));
-    cudaMalloc(&d_V, L * D * sizeof(float));
-    cudaMalloc(&d_output, L * D * sizeof(float));
-    
+    cudaMalloc(&d_Q, B * T * C * sizeof(float));
+    cudaMalloc(&d_K, B * T * C * sizeof(float));
+    // cudaMalloc(&d_V, L * D * sizeof(float));
+    cudaMalloc(&d_output, B * T * T * NH * sizeof(float));
+
     // Copy data to device
-    cudaMemcpy(d_Q, h_Q, L * D * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_K, h_K, L * D * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_V, h_V, L * D * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Q, h_q, B * T * C * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_K, h_k, B * T * C * sizeof(float), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_V, h_V, L * D * sizeof(float), cudaMemcpyHostToDevice);
 
-    attention_forward(d_Q, d_K, d_V, d_output, L, D);
+    // attention_forward(d_Q, d_K, d_V, d_output, L, D);
+    scaled_batched_matmul<<<1, B * T * T * NH>>>(d_Q, d_K, d_output, B, T, C, NH, 1.0);
 
-    cudaMemcpy(h_output, d_output, L * D * sizeof(float), cudaMemcpyDeviceToHost);
-    
+    cudaMemcpy(h_output, d_output, B * T * T * NH * sizeof(float), cudaMemcpyDeviceToHost);
+
     std::cout << "Result matrix:" << std::endl;
-    for (int i = 0; i < L; ++i) {
-        for (int j = 0; j < D; ++j) {
-            std::cout << h_output[i * D + j] << " ";
-        }
-        std::cout << std::endl;
+    // for (int i = 0; i < L; ++i)
+    // {
+    //     for (int j = 0; j < D; ++j)
+    //     {
+    //         std::cout << h_output[i * D + j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    for (int j = 0; j < B * T * T * NH; ++j)
+    {
+        std::cout << h_output[j] << " ";
     }
-    
+
     // Free device memory
-    cudaFree(d_Q);
-    cudaFree(d_K);
-    cudaFree(d_V);
+    cudaFree(d_q);
+    cudaFree(d_k);
+    // cudaFree(d_V);
     cudaFree(d_output);
-    
+
     return 0;
-    
-    }
-    
+}
