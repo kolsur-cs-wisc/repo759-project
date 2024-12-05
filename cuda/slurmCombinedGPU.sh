fbc55fd756376b303e1d3668a473730ca85b8adb
Aditya Kolsur <kolsur@wisc.edu>
2024-12-05 16:17:56 -0600

    Profile GPU kernel

--- cuda/slurmCombinedGPU.sh
+++ cuda/slurmCombinedGPU.sh
@@ -10,25 +10,25 @@
 
 module load gcc/12.2.0
 module load nvidia/cuda/12.2.0
-nvidi-smi
+nvidia-smi
 
 cd $SLURM_SUBMIT_DIR
 nvcc cuda_attention_evaluation.cu matmul.cu softmax.cu attention.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o attn
 
-for i in {1..20};
+for i in {1..11};
 do
     n=$((2**$i))
     echo "2^$i $n"
-    ./attn 128 $n 512 8
+    ncu -o "attn_$n" ./attn 64 $n 512 8
 done
 echo "Attn GPU Finished"
 
 nvcc cuda_flashattention_evaluation.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o flash
 
-for i in {1..20};
+for i in {1..11};
 do
     n=$((2**$i))
     echo "2^$i $n"
-    ./flash 128 $n 512 8
+    ncu -o "flash_$n" ./flash 64 $n 512 8
 done
 echo "Flash GPU Finished"
